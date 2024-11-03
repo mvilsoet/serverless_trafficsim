0e12e170a633bb9238cf8ebabd7640fc3b59e6f0
Michael Vilsoet <57650580+mvilsoet@users.noreply.github.com>
2024-11-03 15:50:53 -0500

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in."
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "dynamodb_table_name" {
+  description = "The name of the DynamoDB table for traffic simulation data."
+  type        = string
+  default     = "TrafficSimulation"
+}
+
+variable "ecr_repository_name" {
+  description = "The name of the ECR repository for Lambda function images."
+  type        = string
+  default     = "traffic-simulation-lambda-repo"
+}
