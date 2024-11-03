008233dfef1da66160a0d5f37985544099ea85ef
Michael Vilsoet <57650580+mvilsoet@users.noreply.github.com>
2024-11-03 15:09:45 -0500

    tf has new lambda repos

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# DynamoDB Table for storing simulation data
 resource "aws_dynamodb_table" "traffic_simulation" {
   name           = "TrafficSimulation"
-  hash_key       = "entity_id"
   billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "entity_id"
 
   attribute {
     name = "entity_id"
@@ -19,6 +20,17 @@ resource "aws_dynamodb_table" "traffic_simulation" {
   }
 }
 
+# ECR Repository for Lambda container images
+resource "aws_ecr_repository" "lambda_repository" {
+  name = "traffic-simulation-lambda-repo"
+}
+
+output "ecr_repository_name" {
+  value       = aws_ecr_repository.lambda_repository.name
+  description = "The name of the ECR repository for Lambda functions."
+}
+
+# SQS Queues for inter-module communication
 resource "aws_sqs_queue" "vehicle_trajectory_queue" {
   name = "VehicleTrajectoryQueue"
 }
@@ -27,7 +39,7 @@ resource "aws_sqs_queue" "traffic_light_queue" {
   name = "TrafficLightQueue"
 }
 
-# Create IAM role and policy for Lambda
+# IAM Role for Lambda Functions
 resource "aws_iam_role" "lambda_exec_role" {
   name = "LambdaExecutionRole"
   assume_role_policy = jsonencode({
@@ -44,47 +56,108 @@ resource "aws_iam_role" "lambda_exec_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+# Attach policies to allow Lambda access to DynamoDB, SQS, and CloudWatch Logs
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_sqs_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs_policy" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# Lambda functions
-resource "aws_lambda_function" "vehicle_trajectory" {
-  function_name = "VehicleTrajectoryLambda"
+# Lambda Functions
+resource "aws_lambda_function" "trajectory" {
+  function_name = "trajectory"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "lambda_handler.lambda_handler"
-  runtime       = "python3.8"
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.lambda_repository.repository_url}:trajectory"
+  memory_size   = 128
+  timeout       = 30
+
   environment {
     variables = {
-      DYNAMODB_TABLE               = aws_dynamodb_table.traffic_simulation.name
-      VEHICLE_TRAJECTORY_QUEUE     = aws_sqs_queue.vehicle_trajectory_queue.id
+      DYNAMODB_TABLE           = aws_dynamodb_table.traffic_simulation.name
+      VEHICLE_TRAJECTORY_QUEUE = aws_sqs_queue.vehicle_trajectory_queue.url
     }
   }
 }
 
-# API Gateway
-resource "aws_apigatewayv2_api" "traffic_simulation_api" {
-  name          = "TrafficSimulationAPI"
+resource "aws_lambda_function" "lights" {
+  function_name = "lights"
+  role          = aws_iam_role.lambda_exec_role.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.lambda_repository.repository_url}:lights"
+  memory_size   = 128
+  timeout       = 30
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE       = aws_dynamodb_table.traffic_simulation.name
+      TRAFFIC_LIGHT_QUEUE  = aws_sqs_queue.traffic_light_queue.url
+    }
+  }
+}
+
+resource "aws_lambda_function" "roadValidation" {
+  function_name = "roadValidation"
+  role          = aws_iam_role.lambda_exec_role.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.lambda_repository.repository_url}:roadValidation"
+  memory_size   = 128
+  timeout       = 30
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.traffic_simulation.name
+    }
+  }
+}
+
+resource "aws_lambda_function" "stateDump" {
+  function_name = "stateDump"
+  role          = aws_iam_role.lambda_exec_role.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.lambda_repository.repository_url}:stateDump"
+  memory_size   = 128
+  timeout       = 30
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.traffic_simulation.name
+    }
+  }
+}
+
+# API Gateway for Visualization (State Dump)
+resource "aws_apigatewayv2_api" "state_dump_api" {
+  name          = "StateDumpAPI"
   protocol_type = "HTTP"
 }
 
-resource "aws_apigatewayv2_integration" "visualization_integration" {
-  api_id           = aws_apigatewayv2_api.traffic_simulation_api.id
+resource "aws_apigatewayv2_integration" "state_dump_integration" {
+  api_id           = aws_apigatewayv2_api.state_dump_api.id
   integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.visualization.invoke_arn
+  integration_uri  = aws_lambda_function.stateDump.invoke_arn
 }
 
-resource "aws_apigatewayv2_route" "visualization_route" {
-  api_id    = aws_apigatewayv2_api.traffic_simulation_api.id
-  route_key = "GET /visualize"
-  target    = "integrations/${aws_apigatewayv2_integration.visualization_integration.id}"
+resource "aws_apigatewayv2_route" "state_dump_route" {
+  api_id    = aws_apigatewayv2_api.state_dump_api.id
+  route_key = "GET /stateDump"
+  target    = "integrations/${aws_apigatewayv2_integration.state_dump_integration.id}"
 }
 
+# Grant API Gateway permission to invoke the stateDump Lambda function
 resource "aws_lambda_permission" "allow_apigateway" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.visualization.function_name
+  function_name = aws_lambda_function.stateDump.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_apigatewayv2_api.traffic_simulation_api.execution_arn}/*/*"
+  source_arn    = "${aws_apigatewayv2_api.state_dump_api.execution_arn}/*/*"
 }
