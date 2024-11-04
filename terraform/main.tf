ed0498b31598df870dec22bc04305c3b79f48eae
Michael Vilsoet <mvilsoet@gmail.com>
2024-11-03 19:37:06 -0500

    store state s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,19 @@
-# Provider Configuration
+# Terraform backend configuration (storing state in S3)
+terraform {
+  backend "s3" {
+    bucket = "mvilsoet-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
