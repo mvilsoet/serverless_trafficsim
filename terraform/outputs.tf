0ace9d179358c10d81c75605b6a791a8b8408e65
Michael Vilsoet <57650580+mvilsoet@users.noreply.github.com>
2024-11-04 20:56:58 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,8 +7,3 @@ output "ecr_repository_url" {
   value       = aws_ecr_repository.lambda_repository.repository_url
   description = "The full URL of the ECR repository for Lambda function images."
 }
-
-output "state_dump_api_endpoint" {
-  value       = aws_apigatewayv2_api.state_dump_api.api_endpoint
-  description = "The API Gateway endpoint for the stateDump Lambda function."
-}
