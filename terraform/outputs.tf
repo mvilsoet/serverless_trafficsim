7403c8b95bd1f822351709f2ff7141aed2668d98
Michael Vilsoet <57650580+mvilsoet@users.noreply.github.com>
2024-11-07 19:16:21 -0500

    Delete terraform/outputs.tf

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ecr_repository_name" {
-  value       = aws_ecr_repository.lambda_repository.name
-  description = "The name of the ECR repository for Lambda functions."
-}
-
-output "ecr_repository_url" {
-  value       = aws_ecr_repository.lambda_repository.repository_url
-  description = "The full URL of the ECR repository for Lambda function images."
-}
